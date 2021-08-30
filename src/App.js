12cef2d0a552e8ce02a13fa786d1759e34c70e1e
Melissa Simpson <melsimpson1023@gmail.com>
2021-08-30 14:52:19 -0400

    Created the Header file and imported it in the App.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Header from './components/Header'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello from react</h1>
+    <div className='container'>
+      <Header />
     </div>
   );
 }
