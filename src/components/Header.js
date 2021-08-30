7a4394dab43355fd8e82350049837680e1cfc14b
Melissa Simpson <melsimpson1023@gmail.com>
2021-08-30 15:16:28 -0400

    Added props and propTypes to the header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,25 @@
-const Header = () => {
+import PropTypes from 'prop-types'
+
+const Header = ({ title }) => {
     return (
         <header>
-            <h1>Task Tracker</h1>
+            <h1>{title}</h1>
         </header>
     )
 }
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+// CSS is JSX
+// const headingStyle = {
+//    color: 'red',
+//    backgroundColor: 'black',
+// }
 
 export default Header
