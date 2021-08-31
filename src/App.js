2460f7a5e109f1c8fd6a3639091324142836f6c4
Melissa Simpson <melsimpson1023@gmail.com>
2021-08-31 13:56:08 -0400

    Working on the toggle reminder

--- src/App.js
+++ src/App.js
@@ -29,10 +29,17 @@ function App() {
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
 }
+
+// Toggle Reminder
+const toggleReminder = (id) => {
+  console.log(id)
+}
+
+
   return (
     <div className='container'>
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No Tasks To Show'}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
     </div>
   )
 }
