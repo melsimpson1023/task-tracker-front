ae619dbbcf1f9edd6b87f3029ee07d4890f5b6b4
Melissa Simpson <melsimpson1023@gmail.com>
2021-08-31 12:26:12 -0400

    Created an event in Button.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('Click')
+    }
     return (
-        <header>
+        <header className='header'>
             <h1>{title}</h1>
+            <Button color='green' text='Add' onClick={onClick} />
         </header>
     )
 }
